5fcdb9887f202b9b19ad80c18be152f5269e531b
arima <arijun0307@gmail.com>
2019-11-02 00:38:15 +0900

    update bag topic

--- scripts/bagrec.sh
+++ scripts/bagrec.sh
@@ -14,7 +14,7 @@ CAMERA="/usb_cam/image_raw/compressed"
 REALSENSE="/camera/color/image_raw/compressed /camera/depth/image_rect_raw/compressedDepth /camera/depth/camera_info /camera/color/camera_info"
 LIDAR="/front_laser/scan /rear_laser/scan /first /last"
 GPS="/fix /odom/gps /nmea_sentence /time_reference"
-CMD_VEL="/t_frog/cmd_vel"
+CMD_VEL="/t_frog/cmd_vel /cmd_vel"
 
 echo $LOCALI &
 echo $VELODYNE &
