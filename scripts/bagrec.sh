2741c148d9334087c3507fb91ae6bbe3af29c3b8
arima <arijun0307@gmail.com>
2019-11-08 23:38:13 +0900

    deleted cmd_vel

--- scripts/bagrec.sh
+++ scripts/bagrec.sh
@@ -14,7 +14,6 @@ CAMERA="/usb_cam/image_raw/compressed"
 REALSENSE="/camera/color/image_raw/compressed /camera/depth/image_rect_raw/compressedDepth /camera/depth/camera_info /camera/color/camera_info"
 LIDAR="/front_laser/scan /rear_laser/scan /first /last"
 GPS="/fix /odom/gps /nmea_sentence /time_reference"
-CMD_VEL="/t_frog/cmd_vel /cmd_vel"
 
 echo $LOCALI &
 echo $VELODYNE &
@@ -22,7 +21,6 @@ echo $CAMERA &
 echo $REALSENSE &
 echo $LIDAR &
 echo $GPS &
-echo $CMD_VEL &
 
-/opt/ros/${distro}/bin/rosbag record $LOCALI $VELODYNE $CAMERA $REALSENSE $LIDAR $GPS $CMD_VEL -O ${HOME}/bagfiles/$TIME.bag
+/opt/ros/${distro}/bin/rosbag record $LOCALI $VELODYNE $CAMERA $REALSENSE $LIDAR $GPS -O ${HOME}/bagfiles/$TIME.bag
 wait
